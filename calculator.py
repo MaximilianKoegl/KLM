7e750c76b7c40542eeeabc60e98b8d126cc20c03
MaximilianKoegl <maximilian.koegl@stud.uni-regensburg.de>
2019-06-10 07:28:11 -0500

    Added timer for button press and release on keyboard and with mouse

--- calculator.py
+++ calculator.py
@@ -4,26 +4,32 @@ from PyQt5 import uic, QtWidgets, QtCore, QtGui
 
 class Calculator(QtWidgets.QDialog):
     new_input = QtCore.pyqtSignal(str)
+    timer_logger = QtCore.pyqtSignal(bool, str)
 
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi("calculator.ui", self)
         self.show()
         self.init_buttons()
+        self.timer = QtCore.QTime()
 
     # connect keyboard- and button-input with listener
     def init_buttons(self):
         self.ui.new_input.connect(self.handle_input)
+        self.ui.timer_logger.connect(self.logging_timer)
         for button in self.numbers.children():
-            button.clicked.connect(self.button_clicked(button.text()))
+            button.pressed.connect(self.button_clicked(button.text()))
+            button.released.connect(self.button_released(button.text()))
 
-    def logger(func):
-        def logging(*args):
-            print('Button pressed: '+args[1])
-            func(*args)
-        return logging
+    def logging_timer(self, ev, button):
+        if ev:
+            self.start_measurement()
+            print('Button pressed,'+button)
 
-    @logger
+        else:
+            self.stop_measurement()
+            print('Button released,'+button+","+ str(self.stop_measurement()))
+
     def handle_input(self, input_text):
         text = self.input.toPlainText()
 
@@ -41,7 +47,17 @@ class Calculator(QtWidgets.QDialog):
 
         self.input.setText(text)
 
+    def start_measurement(self):
+        self.timer.start()
+
+    def stop_measurement(self):
+        return self.timer.elapsed()
+
+    def keyReleaseEvent(self, event):
+        self.ui.timer_logger.emit(False, event.text())
+
     def keyPressEvent(self, event):
+        self.ui.timer_logger.emit(True, event.text())
         # validate if input is not in alphabet
         if not event.text().isalpha():
             self.ui.new_input.emit(event.text())
@@ -49,9 +65,14 @@ class Calculator(QtWidgets.QDialog):
     #  inform listener with value of clicked button
     def button_clicked(self, button_text):
         def clicked():
+            self.ui.timer_logger.emit(True, button_text)
             self.ui.new_input.emit(button_text)
         return clicked
 
+    def button_released(self, button_text):
+        def released():
+            self.ui.timer_logger.emit(False, button_text)
+        return released
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
